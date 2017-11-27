a08ed85babeacfdd9d468e9deb47d5cf0926d86b
KousukeFujii_win8.1_NotePC <konkon.317@gmail.com>
2017-11-27 22:46:39 +0900

    追加　ActionSequencerクラス  (もともとActionIconに合った機能を分割)　[desc]
    branch : Main/dev_sub1
    
    [desc]
    今までActionIcon内で行動状態のシーケンス管理をしていたが
    シーケンス管理専用のコンポーネントを作成し　ActionIconから該当部分を削除
    ActionIconはシーケンサーの状態の変化に応じて表示内容を変えるようにした

--- DragonSlayer/Assets/Script/ActionIcon.cs
+++ DragonSlayer/Assets/Script/ActionIcon.cs
@@ -6,40 +6,27 @@ using UnityEngine.UI;
 
 public class ActionIcon : MonoBehaviour
 {
-    enum Mode
-    {
-        PRELIMINARY_BEFORE,
-        MAIN,
-        PRELIMINARY_END,
-    }
+ 
 
     [SerializeField]
     float sizeRate_normal = 0.5f;
     [SerializeField]
     float sizeRate_valid = 1.0f;
 
-    [SerializeField]
-    float time_preliminary_before = 0.3f;
-    [SerializeField]
-    float time_valid = 0.5f;
-    [SerializeField]
-    float time_preliminary_end = 0.1f;
-
-
     public bool Active{ get { return active;} }
-    bool active;
-
-    float time;
+    bool active;  
 
     Image image;
 
-    Mode mode;
-
+    [SerializeField]
+    ActionSequencer actionSequencer;
+    
     private void Awake()
     {
         image = GetComponent<Image>();
-        if(image)
+        if(image && actionSequencer)
         {
+            actionSequencer.SetEventOnChangeMode(OnSequencerChangeMode);
             Deactivate(true);
         }
         else
@@ -54,23 +41,17 @@ public class ActionIcon : MonoBehaviour
         if(!active)
         {
             //状態初期化
-
             image.rectTransform.localScale = new Vector3(1,1,1)*sizeRate_normal;
-            time = 0f;
+          
             active = true;
-            mode = Mode.PRELIMINARY_BEFORE;
-
+          
             //完全不透過
             Color c = image.color;
             c.a = 1;
             image.color = c;
 
             //アイコン画像を設定
-            image.sprite = ap.Sprite;
-            //稼働時間を設定
-            time_preliminary_before = ap.Time_preliminary_before;
-            time_valid = ap.Time_valid;
-            time_preliminary_end = ap.Time_preliminary_end;
+            image.sprite = ap.Sprite;          
         }
     }
 
@@ -84,61 +65,36 @@ public class ActionIcon : MonoBehaviour
             c.a = 0;
             image.color = c;
 
-            time = 0;
             active = false;
         }
     }
 
-    private void Update()
+    void OnSequencerChangeMode(ActionSequencer.Mode newMode, ActionParamater ap)
     {
-        if(active)
-        { 
-            time += Time.deltaTime;//稼働時間を更新
+        //アクションシーケンサーの状態の変化によって表示を変更する
+        //デリゲートとしてアクションシーケンサーに登録して利用する関数
 
-            //比較相手の数値を現在のモードより決定
-            float targetTime = 0;
-            switch (mode)
-            {
-                case Mode.PRELIMINARY_BEFORE:
-                    targetTime = time_preliminary_before;
-                    break;
-                case Mode.MAIN:
-                    targetTime = time_valid;
-                    break;
-                case Mode.PRELIMINARY_END:
-                    targetTime = time_preliminary_end;
-                    break;
-                default:
-                    Debug.Log("バグ");
-                    break;
-            }
+        switch (newMode)
+        {
+            case ActionSequencer.Mode.PRELIMINARY_BEFORE:
+                Activate(ap);
+                break;
 
-            //加工時間が比較対象を超えていたら
-            //モード切替
-            if(time>targetTime)
-            {
-                time -= targetTime;
+            case ActionSequencer.Mode.MAIN:
+                image.rectTransform.localScale = new Vector3(1, 1, 1) * sizeRate_valid;
+                break;
 
-                switch (mode)
-                {
-                    case Mode.PRELIMINARY_BEFORE:
-                        mode = Mode.MAIN;
-                        image.rectTransform.localScale = new Vector3(1, 1, 1) * sizeRate_valid;
-                        break;
-                    case Mode.MAIN:
-                        mode = Mode.PRELIMINARY_END;
-                        image.rectTransform.localScale = new Vector3(1, 1, 1) * sizeRate_normal;
-                        break;
-                    case Mode.PRELIMINARY_END:
-                        Deactivate();//終了　非アクティブへ
-                        break;
-                    default:
-                        Debug.Log("バグ");
-                        break;
-                }
-            }            
+            case ActionSequencer.Mode.PRELIMINARY_END:
+                image.rectTransform.localScale = new Vector3(1, 1, 1) * sizeRate_normal;
+                break;
+
+            case ActionSequencer.Mode.FINISHED:
+                Deactivate();
+                break;
+
+            default:
+                Debug.Log("バグ");
+                break;
         }
     }
-
-    
 }
