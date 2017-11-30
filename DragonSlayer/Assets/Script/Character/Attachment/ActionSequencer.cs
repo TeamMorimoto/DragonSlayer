3afdb653c56f4a0b9944705439a3d6bfcd745f2a
KousukeFujii_win8.1_NotePC <konkon.317@gmail.com>
2017-11-30 22:32:12 +0900

    追加　ActionSequencerから現在の行動パラメータを取得するためのコード
    branch : Main/dev_sub1

--- DragonSlayer/Assets/Script/Character/Attachment/ActionSequencer.cs
+++ DragonSlayer/Assets/Script/Character/Attachment/ActionSequencer.cs
@@ -36,6 +36,7 @@ public class ActionSequencer : MonoBehaviour
 
     
     ActionParamater currentActionParameter;
+    public ActionParamater CurrentActionParamater { get { return currentActionParameter; } }
 
     private void Awake()
     {
