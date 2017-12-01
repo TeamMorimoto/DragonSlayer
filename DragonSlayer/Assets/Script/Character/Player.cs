7dfd7cbdfe4acd47f621e26eea32861669662b95
KousukeFujii_win8.1_NotePC <konkon.317@gmail.com>
2017-12-01 17:53:41 +0900

    追加　プレイヤー　キーボード入力で行動できるように
    branch : Main/dev_sub1

--- DragonSlayer/Assets/Script/Character/Player.cs
+++ DragonSlayer/Assets/Script/Character/Player.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class Player : Character
 {
+    protected override void Update()
+    {
+        base.Update();
 
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            StartAction(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartAction(1);
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            StartAction(2);
+        }
+
+    }
 }
