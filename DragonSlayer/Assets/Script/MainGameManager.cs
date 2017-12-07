2781d6d2c787c70e4482ea85f59db718d211c681
KousukeFujii_win8.1_NotePC <konkon.317@gmail.com>
2017-12-07 19:17:07 +0900

    追加　ゲームマネージャクラス [desc]
    branch : Main/dev_sub3
    
    [desc]
    リスタートさせる関数
    バトルマネージャの状態を見て勝敗が決したらリスタートのボタンを出す

---
+++ DragonSlayer/Assets/Script/MainGameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class MainGameManager : MonoBehaviour
+{        
+    [SerializeField]
+    BalleManager balleManager;
+
+    [SerializeField]
+    GameObject restartButton;
+
+    private void Awake()
+    {
+        bool flag = false;
+
+        if (balleManager == null) flag = true;
+        if (restartButton == null) flag = true;
+
+        if(flag)
+        {
+            this.enabled = false;
+        }
+        restartButton.SetActive(false);
+
+    }
+
+    private void Update()
+    {
+        if(balleManager.IsMatchDeside)
+        {
+            restartButton.SetActive(true);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
